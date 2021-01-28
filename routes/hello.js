30b5c3a019d0904051dc74034cc690b29d5aa069
Dale Alfafara <dralfafa@ucsd.edu>
2021-01-28 03:03:34 -0800

    node.js added

--- routes/hello.js
+++ routes/hello.js
@@ -4,7 +4,14 @@
  */
 
 exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
+  var nameToShow = req.params.userName;
+  console.log("name is " + nameToShow);
+  res.render('hello', {
+  	"name": nameToShow,
+  	"userName" : "drja",
+  	"firstName" : "Dale",
+  	"lastName" : "Alfafara",
+  	"officeHours" : "Tuesday", 
+  	"classes" : ["Cogs120", "Cogs123", "DSGN100"]
   });
 };
